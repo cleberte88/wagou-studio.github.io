48d908fcff58e0a1cbc318d2520d9b55f1ff4ce3
Cleberte Rodrigues <118860186+cleberte88@users.noreply.github.com>
2023-01-24 18:11:29 -0300

    Feature/update originals 2.0 (#32)
    *melhoria da página Originals da Wagou, criando dinamicamente novos cards com descrições.
    *Implementado o início de uma função, onde o usuário poderá clicar em um original e renderizar na tela todo o projeto de criação da ilustração do card.

--- js/wagou-originals.js
+++ js/wagou-originals.js
@@ -1,60 +1,3 @@
-// const imagens = [
-//     { src: '../works-uilson_and_william/teste-1.jpg', alt: 'Image 1', title: 'Title 1', desc: 'teste', date: '00/00/0000' },
-//     { src: '../works-uilson_and_william/teste-2.jpg', alt: 'Image 2', title: 'Title 2', desc: 'teste', date: '00/00/0000' },
-//     { src: '../works-uilson_and_william/teste-3.jpg', alt: 'Image 3', title: 'Title 3', desc: 'teste', date: '00/00/0000' },
-//     { src: '../works-uilson_and_william/teste-4.jpg', alt: 'Image 4', title: 'Title 4', desc: 'teste', date: '00/00/0000' },
-//     { src: '../works-uilson_and_william/teste-5.jpg', alt: 'Image 5', title: 'Title 5', desc: 'teste', date: '00/00/0000' },
-//     { src: '../works-uilson_and_william/teste-6.jpg', alt: 'Image 6', title: 'Title 6', desc: 'teste', date: '00/00/0000' },
-//     { src: '../works-uilson_and_william/teste-7.jpg', alt: 'Image 7', title: 'Title 7', desc: 'teste', date: '00/00/0000' }
-// ];
-
-
-// const container = document.querySelector('.carousel');
-
-// imagens.forEach(function (image) {
-//     var img = document.createElement('img');
-//     img.src = image.src;
-//     img.alt = image.alt;
-//     img.title = image.title;
-//     img.classList.add('original-image');
-
-//     var desc = document.createElement('p');
-//     // desc.classList.add('description-image')
-//     desc.innerHTML = `<h3>${image.title}</h3> <p>${image.desc}</p> <p>${image.date}</p>`;
-
-//     container.appendChild(desc);
-//     container.appendChild(img);
-// });
-
-// window.onload = function () {
-//     let current = 0;
-//     let images = carousel.getElementsByTagName("img");
-//     images[current].style.display = "block";
-
-//     setTimeout(function () {
-//         setInterval(function () {
-//             for (var i = 0; i < images.length; i++) {
-//                 images[i].style.display = "none";
-//             }
-//             current = (current != images.length - 1) ? current + 1 : 0;
-//             images[current].style.display = "block";
-//         }, 2000);
-//     }, 2000)
-// }
-
-// let descriptions = carousel.getElementsByTagName("p");
-//     descriptions[current].style.display = "none";
-
-//     setInterval(function() {
-//         for (var i = 0; i < descriptions.length; i++) {
-//             descriptions[i].style.display = "none";
-//         }
-//         current = (current != images.length - 1) ? current + 1 : 0;
-//         images[current].style.display = "block";
-//         descriptions[current].style.display = "block";
-//     }, 2000);
-
-
 let vitrine = document.createElement('div');
 vitrine.setAttribute('id','vitrine');
 
@@ -62,24 +5,30 @@ vitrine.setAttribute('id','vitrine');
 let items = [
     {
         id:1,
-        name: 'Item 1',
-        description: 'Description of Item 1',
-        image: 'https://via.placeholder.com/150',
+        name: 'William',
+        description: 'Animador e pós produtor da Wagou',
+        image: {
+            src: "../img/william.jpeg"
+        },
         date: '00/00/0001'
     },
     {
         id:2,
-        name: 'Item 2',
-        description: 'Description of Item 2',
-        image: 'https://via.placeholder.com/150',
+        name: 'Wilson',
+        description: 'Animador e desenvolvedor Front-End',
+        image: {
+            src: "../img/foto do wilson.jpg"
+        },
         date: '00/00/0002'
     },
     {
         id:3,
-        name: 'Item 3',
-        description: 'Description of Item 3',
-        image: 'https://via.placeholder.com/150',
-        date: '00/00/0003'
+        name: 'Cleberte',
+        description: 'Desenvolvedor Front-End',
+        image: {
+            src: "../img/cleberte.jpeg"
+        },
+        date: '12/12/2000'
     }
 ]
 
@@ -89,7 +38,7 @@ for(let i = 0; i < items.length; i++) {
     item.setAttribute('class', 'item');
 
     let img = document.createElement('img');
-    img.setAttribute('src', items[i].image);
+    img.setAttribute('src', items[i].image.src);
     img.setAttribute('alt', items[i].name);
     img.setAttribute('class', 'img-item');
     item.appendChild(img);
@@ -100,31 +49,20 @@ for(let i = 0; i < items.length; i++) {
     info.style.display = 'none';
 
     let title = document.createElement('h3');
-    // title.classList.add('description-image')
     let titleText = document.createTextNode(items[i].name);
     title.appendChild(titleText);
     info.appendChild(title);
 
     let desc = document.createElement('p');
-    // desc.classList.add('description-image')
     let descText = document.createTextNode(items[i].description);
     desc.appendChild(descText);
     info.appendChild(desc);
 
     let date = document.createElement('p');
-    // date.classList.add('description-image')
-    // link.setAttribute('href',`/item/${ites[i].id}`);
     let linkText = document.createTextNode(items[i].date);
     date.appendChild(linkText);
     info.appendChild(date);
     item.appendChild(info);
-
-
-    // let divAll = document.createElement('div');
-    // divAll.classList.add('description-image');
-    // divAll.appendChild(title);
-    // divAll.appendChild(desc);
-    // divAll.appendChild(date);
 
     item.addEventListener('mouseover', function() {
         item.querySelector('.info').style.display = 'block';
@@ -159,3 +97,85 @@ setInterval(showNextItem, 2000);
 
 let container = document.getElementById('carousel');
 container.appendChild(vitrine);
+
+
+
+//criação dinâmica da parte de imagens dispostas da página
+
+let imgDesc = [
+    {
+        id: 1,
+        name: 'Item 1',
+        description: 'Teste de descrição do original',
+        image: '../img/cleberte.jpeg'
+    },
+    {
+        id: 2,
+        name: 'Item 2',
+        description: 'Description of Item 2',
+        image: '../img/foto do wilson.jpg'
+    },
+    {
+        id: 3,
+        name: 'Item 3',
+        description: 'Description of Item 3',
+        image: '../img/william.jpeg'
+    }
+];
+
+let section = document.getElementById('container-originals-content');
+let divAll = document.querySelector('.divAll-content');
+let title = document.createElement('h2');
+title.setAttribute('class', 'title-section')
+title.innerHTML = 'Mais sobre nossos originals';
+section.appendChild(title);
+section.appendChild(divAll);
+
+imgDesc.forEach(function(item, index) {
+    let div = document.createElement('div');
+    div.setAttribute('class', 'div-img-desc');
+    let img = document.createElement('img');
+    let p = document.createElement('p');
+
+    img.src = item.image;
+    img.alt = item.name;
+    img.onclick = function (){
+        if(index == 0){
+            let container = document.querySelector('.container-originals');
+            container.style.display = 'none';
+            alert('Index 0 funcionou');
+        }
+        else if(index == 1){
+            let container = document.querySelector('.container-originals');
+            container.style.display = 'none';
+            alert('Index 1 funcionou');
+        }else{
+            let container = document.querySelector('.container-originals');
+            container.style.display = 'none';
+            alert('Index 3 funcionou');
+        }
+    }
+    p.onclick = function (){
+        if(index == 0){
+            let container = document.querySelector('.container-originals');
+            container.style.display = 'none';
+            alert('Index 0 funcionou');
+        }
+        else if(index == 1){
+            let container = document.querySelector('.container-originals');
+            container.style.display = 'none';
+            alert('Index 1 funcionou');
+        }else{
+            let container = document.querySelector('.container-originals');
+            container.style.display = 'none';
+            alert('Index 3 funcionou');
+        }
+    }
+
+    
+    p.innerHTML = item.description;
+
+    div.appendChild(img);
+    div.appendChild(p);
+    divAll.appendChild(div);
+});
